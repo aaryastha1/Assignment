e8aa7bcce6544b13b4e0ddb21e321a2d862d7e09
aaryastha1 <shresthaariya7@gmail.com>
2024-06-17 16:30:16 +0545

    main

--- app/src/main/java/com/example/assignment/ui/RegistrationActivity.kt
+++ app/src/main/java/com/example/assignment/ui/RegistrationActivity.kt
@@ -1,8 +1,10 @@
 package com.example.assignment.ui
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
@@ -15,17 +17,36 @@ import com.example.userauthentication.Model.UserModel
 import com.example.userauthentication.Repository.UserRepositoryImpl
 import com.example.userauthentication.Utils.ImageUtils
 import com.example.userauthentication.ViewModel.UserViewModel
+import com.google.firebase.database.FirebaseDatabase
 import com.squareup.picasso.Picasso
 import java.util.UUID
 
 class RegistrationActivity : AppCompatActivity() {
     lateinit var registrationBinding: ActivityRegistration2Binding
 
+    var firebaseDatabase: FirebaseDatabase= FirebaseDatabase.getInstance()
+    var ref = firebaseDatabase.reference.child("user")
+
+
     lateinit var activityResultLauncher : ActivityResultLauncher<Intent>
     var imageUri : Uri? = null
 
     lateinit var imageUtils: ImageUtils
     lateinit var userViewmodel : UserViewModel
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == 1 && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            activityResultLauncher.launch(intent)
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -56,16 +77,13 @@ class RegistrationActivity : AppCompatActivity() {
             }
         }
 
+
         registrationBinding.buttonregister2.setOnClickListener {
-            var name : String = registrationBinding.editname.text.toString()
-            var email : String = registrationBinding.editemail.text.toString()
-            var number : Int = registrationBinding.editNumber.text.toString().toInt()
-            var password : String = registrationBinding.editPassword.text.toString()
+            uploadImage()
 
             Toast.makeText(applicationContext, "Registration Success", Toast.LENGTH_LONG).show()
         }
 
-        setContentView(R.layout.activity_registration2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -85,11 +103,22 @@ class RegistrationActivity : AppCompatActivity() {
 
     }
     fun addUser(url:String, imageName: String){
+        Log.d("errroeroeoreroe","i am called herer")
         var name : String = registrationBinding.editname.text.toString()
         var email : String = registrationBinding.editemail.text.toString()
         var number : Int = registrationBinding.editNumber.text.toString().toInt()
         var password : String = registrationBinding.editPassword.text.toString()
         var data = UserModel("",name,email,number,password,url,imageName)
+
+        userViewmodel.addUser(data) { success, message ->
+            if (success) {
+                Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+
+            }
+        }
+
 
 
     }
