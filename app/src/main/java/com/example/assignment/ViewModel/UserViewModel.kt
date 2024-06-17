db147162639f02801527a2331c08503470f631c9
aaryastha1 <shresthaariya7@gmail.com>
2024-06-17 11:02:52 +0545

    main

--- app/src/main/java/com/example/assignment/ViewModel/UserViewModel.kt
+++ app/src/main/java/com/example/assignment/ViewModel/UserViewModel.kt
@@ -1,4 +1,49 @@
-package com.example.assignment.ViewModel
+package com.example.userauthentication.ViewModel
 
-class UserViewModel {
+import android.net.Uri
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.userauthentication.Model.UserModel
+import com.example.userauthentication.Repository.UserRepository
+
+class UserViewModel(val repository: UserRepository):ViewModel(){
+    fun deleteData(id:String,callback:(Boolean,String?) -> Unit){
+        repository.deleteData(id,callback)
+    }
+    fun deleteImage(imageName:String,callback:(Boolean,String?) -> Unit){
+        repository.deleteImage(imageName,callback)
+    }
+
+
+    fun updateUser(id:String,data: MutableMap<String,Any>?,callback:(Boolean,String?) -> Unit){
+        repository.UpdateUser(id,data,callback)
+    }
+
+    fun addUser(userModel: UserModel, callback: (Boolean, String?) -> Unit) {
+        repository.addUser(userModel, callback)
+    }
+
+    fun uploadImage(imageName: String, imageUrl: Uri, callback: (Boolean, String?) -> Unit) {
+        repository.uploadImage(imageName, imageUrl){success,imageUrl ->
+            callback(success, imageUrl)
+        }
+    }
+
+    private var _userList = MutableLiveData<List<UserModel>?>()
+    var userList = MutableLiveData<List<UserModel>?>()
+        get() = _userList
+
+    var _loadingState = MutableLiveData<Boolean>()
+    var loadingState = MutableLiveData<Boolean>()
+        get() = _loadingState
+
+    fun fetchUser() {
+        _loadingState.value = true
+        repository.getAllUser() { userList, success, message ->
+            if (userList != null) {
+                _loadingState.value= false
+                _userList.value = userList
+            }
+        }
+    }
 }
